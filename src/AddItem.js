1708fce22ee9997094cf793c5a09bc5727737b0b
kumaraswamy20 <46063637+kumaraswamy20@users.noreply.github.com>
2019-08-25 19:39:06 +0530

    Add files via upload

---
+++ src/AddItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+    render() {
+        if (this.props.flag) {
+            return (
+                <div style={{ marginLeft: '30%', marginRight: '30%', width: '40%', textAlign: "left" }}>
+                    <div>
+                        Work Item <input type="text" className="form-control" name="workItem" value={this.props.workItem} onChange={this.props.changeText} />
+                    </div>
+                    <div>
+                        Due Date <input type="date" className="form-control" name="dueDate" value={this.props.dueDate} onChange={this.props.changeText} />
+                    </div>
+                    <div>
+                        Resources <input type="number" className="form-control" name="resources" value={this.props.resources} onChange={this.props.changeText} />
+                    </div>
+                    <br />
+                    <div style={{ textAlign: "center" }}>
+                        {!this.props.id && <button className="btn btn-success" onClick={this.props.create}>Create</button>}
+                        {this.props.id && <button className="btn btn-success" onClick={this.props.update}>Update</button>}&nbsp;&nbsp;
+                        {<button className="btn btn-warning" onClick={this.props.close}>Close</button>}
+                    </div>
+                </div>
+            );
+        }
+        else
+            return null;
+    }
+}
+
+export default AddItem;
---
+++ src/Items.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap'
+import AddItem from './AddItem';
+
+export default class Items extends Component {
+    constructor() {
+        super();
+        this.state = {
+            flag: 0,
+            id: '',
+            workItem: '',
+            dueDate: '',
+            resources: '',
+            editId: undefined,
+            items: []
+        }
+    }
+    
+    componentDidMount() {
+        const items = localStorage.getItem("items");
+        this.setState({ items: JSON.parse(items) });
+    }
+
+    changeText = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    createNewItem = () => {
+        let item = {
+            id: Math.floor(Math.random() * 100),
+            workItem: this.state.workItem,
+            dueDate: this.state.dueDate,
+            resources: this.state.resources
+        }
+        const items = this.state.items.concat([item]);
+        this.setState({
+            items,
+            flag: 0,
+            workItem: '',
+            dueDate: '',
+            resources: ''
+        });
+        localStorage.setItem("items", JSON.stringify(items));
+        localStorage.setItem("workItems", items.length);
+    }
+
+    editItem = (event) => {
+        const editItem = this.state.items.find(item => parseInt(event.target.id) === item.id);
+        this.setState({
+            flag: 1,
+            workItem: editItem.workItem,
+            dueDate: editItem.dueDate,
+            resources: editItem.resources,
+            editId: editItem.id
+        });
+    }
+
+    updateItem = () => {
+        let editItem = {
+            id: this.state.editId,
+            workItem: this.state.workItem,
+            dueDate: this.state.dueDate,
+            resources: this.state.resources
+        }
+        const items = this.state.items.map(item =>
+            item.id === editItem.id ? editItem : item
+        )
+        this.setState({
+            items,
+            flag: 0,
+            workItem: '',
+            dueDate: '',
+            resources: '',
+            editId: undefined
+        });
+        localStorage.setItem("items", JSON.stringify(items));
+        localStorage.setItem("workItems", items.length);
+    }
+
+    deleteItem = (event) => {
+        if (window.confirm("are you sure want to delete")) {
+            const items = this.state.items.filter(item => parseInt(event.target.id) !== item.id);
+            this.setState({ items });
+            localStorage.setItem("items", JSON.stringify(items));
+            localStorage.setItem("workItems", items.length);
+        }
+    }
+
+    handleClose = () => {
+        this.setState({ flag: 0 });
+    }
+
+    showPopup = () => {
+        this.setState({ flag: 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <br />
+                <Row>
+                    <Col md={2}></Col>
+                    <Col md={2}>
+                        <h3>React Assignment</h3>
+                    </Col>
+                    <Col md={3}></Col>
+                    <Col md={3}>
+                        <h4>Number of Work Items: {localStorage.getItem("workItems")}</h4>
+                    </Col>
+                    <Col md={2}></Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col md={5}></Col>
+                    <Col md={2}>
+                        <button className="btn btn-success" >Upload to Google Spread Sheet</button>
+                    </Col>
+                    <Col md={2}>
+                        <button className="btn btn-success" onClick={this.showPopup}>Add New Item</button>
+                    </Col>
+                    <Col md={3}></Col>
+                </Row>
+                <br /> 
+                <Row>
+                    <Col md={3}></Col>
+                    <Col md={6}>
+                        <table className="table" style={{border:'1px solid'}}>
+                            <thead> 
+                                <tr>
+                                    <th style={{textAlign:"center"}}>Id</th>
+                                    <th style={{textAlign:"center"}}>WorkItem</th>
+                                    <th style={{textAlign:"center"}}>Due Date</th>
+                                    <th style={{textAlign:"center"}}>Resources</th>
+                                    <th style={{textAlign:"center"}}>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.items.map((item) => {
+                                        return (
+                                            <tr key>
+                                                <td>{item.id}</td>
+                                                <td>{item.workItem}</td>
+                                                <td>{item.dueDate}</td>
+                                                <td>{item.resources}</td>
+                                                <td>
+                                                    <i className="fas fa-edit" id={item.id} onClick={this.editItem}></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                    <i className="fas fa-trash" id={item.id} onClick={this.deleteItem}></i>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </Col>
+                    <Col md={3}></Col>
+                </Row>
+                
+                <br />
+                <AddItem
+                    id={this.state.editId}
+                    workItem={this.state.workItem}
+                    dueDate={this.state.dueDate}
+                    resources={this.state.resources}
+                    flag={this.state.flag}
+                    changeText={this.changeText}
+                    create={this.createNewItem}
+                    update={this.updateItem}
+                    close={this.handleClose}
+                />
+            </div>
+        );
+    }
+}
